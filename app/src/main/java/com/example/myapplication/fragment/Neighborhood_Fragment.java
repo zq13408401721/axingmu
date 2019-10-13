8f9461856b4d3b17986034ec4f97786450a613d2
Long778899 <1770166018@qq.com>
2019-10-13 19:02:28 +0800

    q

--- app/src/main/java/com/example/myapplication/fragment/Neighborhood_Fragment.java
+++ app/src/main/java/com/example/myapplication/fragment/Neighborhood_Fragment.java
@@ -1,25 +1,38 @@
 package com.example.myapplication.fragment;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.adaper.NeighborhoodRec_Adapter;
 import com.example.myapplication.base.BaseFragment;
+import com.example.myapplication.fragment.neighbor.ContactsActivity;
 import com.example.myapplication.interfaces.IBasePresenter;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 public class Neighborhood_Fragment extends BaseFragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_neighborhood_, container, false);
-    }
+    @BindView(R.id.neighbor_rec)
+    RecyclerView neighborRec;
+    Unbinder unbinder;
+    @BindView(R.id.address_secrecy)
+    ImageView addressSecrecy;
+    @BindView(R.id.address_more)
+    ImageView addressMore;
+    Unbinder unbinder1;
+
 
     @Override
     protected IBasePresenter getPresenter() {
@@ -28,7 +41,49 @@ public class Neighborhood_Fragment extends BaseFragment {
 
     @Override
     protected int getLayoutId() {
-        return 0;
+        return R.layout.fragment_neighborhood_;
     }
 
+    @Override
+    protected void initView() {
+        super.initView();
+        neighborRec.setLayoutManager(new LinearLayoutManager(getContext()));
+        ArrayList<String> strings = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            strings.add("asd");
+        }
+        NeighborhoodRec_Adapter neighborhoodRec_adapter = new NeighborhoodRec_Adapter(strings);
+        neighborRec.setAdapter(neighborhoodRec_adapter);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder1 = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder1.unbind();
+    }
+
+    @OnClick({R.id.address_secrecy, R.id.address_more})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.address_secrecy:
+                break;
+            case R.id.address_more:
+                Intent intent = new Intent(getContext(), ContactsActivity.class);
+
+                startActivity(intent);
+
+                break;
+        }
+    }
 }
